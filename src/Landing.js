b27f75d5517453649b8a2eaf713de3ff94058ced
Noah <bitterhippo@gmail.com>
2021-09-24 22:48:26 +0900

    RRD + basic styling

--- src/Landing.js
+++ src/Landing.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import Home from "./Screens/Home";
+import About from "./Screens/About";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 const Landing = () => {
   return (
-    <h1>
-      LOL
-    </h1>
+    <div style={styles.wrapper}>
+      <h1 style={styles.title}>Noah's Portfolio</h1>
+      <Router>
+        <Switch>
+          <Route exact component={Home} path="/" />
+          <Route exact component={About} path="/about" />
+        </Switch>
+      </Router>
+    </div>
   )
 };
+
+const styles = {
+  title: {
+    paddingTop: '5vh',
+    textAlign: 'center'
+  },
+  wrapper: {
+    backgroundColor: 'black',
+    color: 'white',
+    opacity: 0.9,
+    width: '90%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    paddingTop: 0,
+    minHeight: '100vh',
+    paddingBottom: '5vh'
+  }
+}
 
 export default Landing;
