f04b62470b828532fcfdb08bbaec6cf45ab9849f
Noah <bitterhippo@gmail.com>
2021-10-26 19:28:13 +0900

    small change to dummy data

--- src/Components/DummyData.js
+++ src/Components/DummyData.js
@@ -12,46 +12,46 @@ let DummyData = {
   },
   Physiotherapy: {
     Howitworks: {
-      text: "This is Chiropractic how it works data"
+      text: "This is Physiotherapy how it works data"
     },
     Howithelps: {
-      text: "This is Chiropractic how it helps data"
+      text: "This is Physiotherapy how it helps data"
     },
     Whatittreats: {
-      text: "This is Chiropractic what it treats data"
+      text: "This is Physiotherapy what it treats data"
     }
   },
   TherapueticUltrasound: {
     Howitworks: {
-      text: "This is Chiropractic how it works data"
+      text: "This is TherapueticUltrasound how it works data"
     },
     Howithelps: {
-      text: "This is Chiropractic how it helps data"
+      text: "This is TherapueticUltrasound how it helps data"
     },
     Whatittreats: {
-      text: "This is Chiropractic what it treats data"
+      text: "This is TherapueticUltrasound what it treats data"
     }
   },
   FunctionalRehab: {
     Howitworks: {
-      text: "This is Chiropractic how it works data"
+      text: "This is FunctionalRehab how it works data"
     },
     Howithelps: {
-      text: "This is Chiropractic how it helps data"
+      text: "This is FunctionalRehab how it helps data"
     },
     Whatittreats: {
-      text: "This is Chiropractic what it treats data"
+      text: "This is FunctionalRehab what it treats data"
     }
   },
   NutritionalConsulting: {
     Howitworks: {
-      text: "This is Chiropractic how it works data"
+      text: "This is NutritionalConsulting how it works data"
     },
     Howithelps: {
-      text: "This is Chiropractic how it helps data"
+      text: "This is NutritionalConsulting how it helps data"
     },
     Whatittreats: {
-      text: "This is Chiropractic what it treats data"
+      text: "This is NutritionalConsulting what it treats data"
     }
   }
 };
