0509405b27626b5e06fed0d0a9838a45f310cfec
Noah <bitterhippo@gmail.com>
2021-11-01 08:37:27 +0900

    Update About.js

--- src/Screens/About.js
+++ src/Screens/About.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ContentBlock from '../Components/Content/ContentBlock';
 import BannerImg from '../Components/ImgContainer/BannerImg';
 
 const About = () => {
@@ -29,25 +28,38 @@ const About = () => {
         image={'CeaserTeam'}
         message="About us"
       />
-      <div>
-        <span style={styles.bigText}>Our Process</span>
+      <div style={styles.splitView}>
+        {/* First Screen */}
+        <div style={styles.screen}>
+          <div>
+            <span style={styles.bigText}>Our Process</span>
+          </div>
+          <div>
+            <span style={styles.boldText}>An Informed Examination: </span>This will take place on your first visit, so that we can make or confirm a diagnosis, and udnerstand the state and stage of your injury. We'll check your medical history, find out about your level of physical activity and any relevant lifestyle factors.
+          </div>
+          <div>
+            <span style={styles.boldText}>A Professional Diagnosis: </span>We'll diagnose the source of your injury, and establish whether it has caused any knock-on effects within your body. Your physical therapist will also help you understand your condition.
+          </div>
+          <div>
+            <span style={styles.boldText}>A Unique Treatman Plan: </span>We've learned in our practice that no two patients are exactly alike, which is why your unique history, condition and hgoals will be reflected in the personalized treatment plan we plut together for you. This plan includes: skilled "hands on" treatments such as soft tissue-mobilization, acrupressure and manipulation, laser, ultrasound, electrical currents, acupuncture, and heat or ice to relieve, individuality, prescribed exercise programs.
+          </div>
+          <div>
+            <span style={styles.boldText}>Home Improvement Assignements: </span>As part of your treatment plan, we are also likely to assign you home exercises to help you heal faster, strengthen your system and achieve your goals.
+          </div>
+          <button style={styles.button}>Book an Initial Assessment</button>
+        </div>
+        {/* Second Screen */}
+        <div>
+          <div style={styles.imgHolder}>
+
+          </div>
+        </div>
       </div>
-      <div>
-        <span style={styles.boldText}>An Informed Examination: </span>This will take place on your first visit, so that we can make or confirm a diagnosis, and udnerstand the state and stage of your injury. We'll check your medical history, find out about your level of physical activity and any relevant lifestyle factors.
-      </div>
-      <div>
-        <span style={styles.boldText}>A Professional Diagnosis: </span>We'll diagnose the source of your injury, and establish whether it has caused any knock-on effects within your body. Your physical therapist will also help you understand your condition.
-      </div>
-      <div>
-        <span style={styles.boldText}>A Unique Treatman Plan: </span>We've learned in our practice that no two patients are exactly alike, which is why your unique history, condition and hgoals will be reflected in the personalized treatment plan we plut together for you. This plan includes: skilled "hands on" treatments such as soft tissue-mobilization, acrupressure and manipulation, laser, ultrasound, electrical currents, acupuncture, and heat or ice to relieve, individuality, prescribed exercise programs.
-      </div>
-      <div>
-        <span style={styles.boldText}>Home Improvement Assignements: </span>As part of your treatment plan, we are also likely to assign you home exercises to help you heal faster, strengthen your system and achieve your goals.
-      </div>
-      <button style={styles.button}>Book an Appointment</button>
       <div style={StyleSheet.staffList}>
         {
-          staffList.map(currentMember => <div>
+          staffList.map(currentMember => <div 
+            style={styles.staffContainer}
+          >
             {currentMember.name}
             <button style={StyleSheet.staffListButton}></button>
           </div>)
@@ -61,9 +73,6 @@ const About = () => {
 };
 
 const styles = {
-  aboutScreenWrapepr: {
-
-  },
   staffList: {
     display: 'flex',
     flexDirection: 'inline-row',
@@ -81,8 +90,32 @@ const styles = {
     fontWeight: "bold",
   },
   button: {
+    marginTop: '20px',
     backgroundColor: 'green',
     color: 'white',
+    borderRadius: '5px',
+    padding: '5px 20px 5px 20px',
+  },
+  splitView: {
+    display: 'flex',
+    flexDirection: 'inline-row',
+    marginTop: '40px',
+    gap: '20px',
+  },
+  screen: {
+    width: '50%',
+    marginTop: '30px',
+    marginLeft: '20px',
+  },
+  imgHolder: {
+    marginTop: '70px',
+    height: '300px',
+    width: '300px',
+    backgroundColor: 'gray'
+  },
+  staffContainer: {
+    display: 'flex',
+    flexDirection: 'column',
   }
 }
 
