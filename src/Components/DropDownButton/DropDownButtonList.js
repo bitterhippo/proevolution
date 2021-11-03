283f82cde29a759a07ec0b810568dffec15546c3
Noah <bitterhippo@gmail.com>
2021-11-03 22:36:44 +0900

    Create DropDownButtonList.js

---
+++ src/Components/DropDownButton/DropDownButtonList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import DropDownButton from './DropDownButton';
+
+const DropDownButtonList = ({ listData }) => {
+  return (
+    <div style={styles.dropDownListContainer}>
+      {
+        listData.map(currentItem => <DropDownButton 
+          title={currentItem.title}
+          content={currentItem.content}
+        />)
+      }
+    </div>
+  )
+};
+
+const styles = {
+  dropDownListContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 15
+  }
+};
+
+return DropDownButtonList;
