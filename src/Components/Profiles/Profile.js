4738205496d720d2b35a8591fb4b5588f566e805
Noah <bitterhippo@gmail.com>
2021-11-10 00:49:25 +0900

    yooo

--- src/Components/Profiles/Profile.js
+++ src/Components/Profiles/Profile.js
@@ -2,14 +2,31 @@ import React from 'react';
 
 const Profile = ({ image, name, link }) => {
   return (
-    <div>
-
+    <div style={styles.profileWrapper}>
+      {/* Place holder box for images */}
+      <div style={styles.imgHolder}>
+      </div>
+      <div style={styles.text}>
+        { name }
+      </div>
     </div>
   )
 };
 
 const styles = {
-
+  imgHolder: {
+    height: 125,
+    weight: 125,
+    backgroundColor: 'gray'
+  },
+  profileWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: 'red'
+  },
+  text: {
+    textAlign: 'center'
+  }
 };
 
 export default Profile;
---
+++ src/Components/Profiles/ProfileList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProfileList = ({ listData }) => {
+  return (
+    <div>
+
+    </div>
+  )
+};
+
+const styles = {
+
+};
+
+export default ProfileList;
