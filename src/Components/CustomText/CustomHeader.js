9304d500eff38a882641d23a0d9648777162a3b4
Noah <bitterhippo@gmail.com>
2021-12-18 23:56:02 +0900

    Create CustomHeader.js

---
+++ src/Components/CustomText/CustomHeader.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Colors from '../Colors';
+
+const CustomHeader = (props) => {
+  return (
+    <span style={styles.headerText}>
+      { props.children }
+    </span>
+  )
+};
+
+const styles = {
+  headerText: {
+    fontSize: "30px",
+    fontWeight: "bold",
+    color: Colors.Primary
+  }
+};
+
+export default CustomHeader;
