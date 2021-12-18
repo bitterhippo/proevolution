0fe257001d5990a2e22b5c077e91c26142abca0a
Noah <bitterhippo@gmail.com>
2021-12-18 23:09:15 +0900

    Update CustomButton.js

--- src/Components/CustomButton/CustomButton.js
+++ src/Components/CustomButton/CustomButton.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { ProgressPlugin } from 'webpack';
 import Colors from '../../Components/Colors';
 
-const CustomButton = ({ buttonName, padding, margin }) => {
+const CustomButton = ({ buttonName, padding, margin, props }) => {
   return (
     <button style={styles.button}>
-
+      {props.children}
     </button>
   )
 };
@@ -19,3 +20,5 @@ const styles = {
     filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))'
   }
 };
+
+export default CustomButton;
