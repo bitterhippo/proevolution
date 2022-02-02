813fc213623e85fef2e3b522edff616e35a8390d
Noah <bitterhippo@gmail.com>
2022-02-02 23:34:07 +0900

    Create DefaultPractionerData.js

---
+++ src/Components/DefaultPractionerData.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const DefaultPractionerData = {
+  Jeremy: {
+    title: `RMT`,
+    content: <>
+      Jeremy has been with Pro Evolution since 2017. He specializes in massage therapy, physiotherapy as well as chiropractic care. He studied at the University of Collingwood where he received top marks in his class.
+      <br></br>
+      What brought Jeremy to this field is his interest in helping people manage discomfort and come up with a treatment plan that can be implemented in their daily life.
+      <br></br>
+      In his free time, Jeremy enjoys indoor rock climbing and animated movies.
+    </>
+  },
+  Vanessa: {
+    title: `RMT`,
+    contet: <>
+      Lisa Andrews joined Pro Evolution after immigrating from Scotland where she had her own practice but was in search of better weather and nature.
+      <br></br>
+      She is a trained physiotherapist having graduated from the University of Western Scotland with a BSC. in Physiotherapy and a MA in Patient Care. She wrote her thesis on “ Trauma Caused by Car Accidents and Lingering Effects.”
+      <br></br>
+      Lisa works with clients that have either suffered a workplace injury, car accident victims or people just experiencing difficulty with their movements. She specializes in using a hands-on approach and provides all her clients with detailed information of all movements they need to perform at home after a session.
+      <br></br>
+      Lisa is an experienced equestrian and loves to explore new trails up north with her horse, Daisy.
+    </>
+  },
+  Lisa: {
+    title: `PT`,
+    content: <>
+      Lisa Andrews joined Pro Evolution after immigrating from Scotland where she had her own practice but was in search of better weather and nature.
+      <br></br>
+      She is a trained physiotherapist having graduated from the University of Western Scotland with a BSC. in Physiotherapy and a MA in Patient Care. She wrote her thesis on “ Trauma Caused by Car Accidents and Lingering Effects.”
+      <br></br>
+      Lisa works with clients that have either suffered a workplace injury, car accident victims or people just experiencing difficulty with their movements. She specializes in using a hands-on approach and provides all her clients with detailed information of all movements they need to perform at home after a session.
+      <br></br>
+      Lisa is an experienced equestrian and loves to explore new trails up north with her horse, Daisy.
+    </>
+  },
+  Mary: {
+    title: 'PT',
+    content: <>
+      Lisa Andrews joined Pro Evolution after immigrating from Scotland where she had her own practice but was in search of better weather and nature.
+      <br></br>
+      She is a trained physiotherapist having graduated from the University of Western Scotland with a BSC. in Physiotherapy and a MA in Patient Care. She wrote her thesis on “ Trauma Caused by Car Accidents and Lingering Effects.”
+      <br></br>
+      Lisa works with clients that have either suffered a workplace injury, car accident victims or people just experiencing difficulty with their movements. She specializes in using a hands-on approach and provides all her clients with detailed information of all movements they need to perform at home after a session.
+      <br></br>
+      Lisa is an experienced equestrian and loves to explore new trails up north with her horse, Daisy.
+    </>
+  }
+};
+
+export default DefaultPractionerData;
